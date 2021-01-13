23d3b122c724693d3786e66a5834748e5b7c3012
dm51 <dmw51@cam.ac.uk>
2021-01-13 13:43:01 +0100

    restructured project files

--- settings.py
+++
@@ -1,7 +0,0 @@
-""" This files allows dynamically creating a reference to the main figure and using it across multiple modules"""
-import numpy as np
-
-global main_figure
-DISTANCE_FN_CHARS = lambda cc: 2.25 * np.max([cc.width, cc.height])
-
-main_figure = []
